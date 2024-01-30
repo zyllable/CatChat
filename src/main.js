a6d6fccc2e7a86b4dd412849efe603e592006397
zyllable <dentalfloss2217@gmail.com>
2024-01-30 10:42:02 -0600

    added spritesheet class

--- src/main.js
+++ src/main.js
@@ -32,6 +32,13 @@ const createImage = (url) => {
 	image.src = url;
 	return image;
 }
+const getIntegerRange = (start, end) => {
+	let range = []
+	for (let i = start; i <= end; i++) {
+		range.push(i);
+	}
+	return range;
+}
 
 //classes
 class Scene {
@@ -81,7 +88,7 @@ class Box {
 	/*
 		A rectangle.
 		ATTRIBUTES
-		c-id=string - the id of the box, to be used in the associative array for collisions
+		c-id=string - the id of the box, to be used in the associative array for collisions -- can be left undefined when not needed
 		c-x=number - the x position
 		c-y=number - the y position
 		c-width=number = the width
@@ -164,6 +171,124 @@ class Sprite extends Box {
 		context.drawImage(this.image, this.x, this.y, this.width, this.height);
 	}
 }
+class SpriteSheet { //refer to external documentation for how this thing works, way too long to explain in comments
+	/*
+		A way of importing a spritesheet and sectioning it into groups that can be animated
+		ATTRIBUTES
+		c-image=image - the image object to be used
+		c-frameWidth=number - the width of each frame
+		c-frameHeight=number - the height of each frame
+		frameGroups=array=array=box - groups of frames
+		currentFrame=box - the current frame shown
+		currentFrameGroup=number
+		currentFrameIndex=number
+		METHODS
+		nextFrame() - switches to the next frame in the group, returns true if ends at frame 0
+		previousFrame() - switches to the previous frame in the, returns true if ends at frame 0
+		toFrame(frame) - switches to the specified frame
+		switchGroup(index) - switches to the specified group of frames on the spritesheet
+	*/
+	frameGroups = [];
+	currentFrameGroup = 0;
+	currentFrameIndex = 0;
+
+	constructor(image, frameWidth, frameHeight, ...ranges) {
+		this.image = image;
+		let width = image.width;
+		let height = image.height;
+		let columns = Math.floor(width / frameWidth)
+		let rows = Math.floor(height / frameHeight);
+		if (!columns || !rows) {throw Error(`Image too small for specified frame height/width\nImage size: ${width}x${height}\nFrame size: ${frameWidth}x${frameHeight}`)}
+		let i = 0;
+		for (let range of ranges) {
+			this.frameGroups[i] = [];
+			let ii = 0;
+			const frames = getIntegerRange(range[0], range[1]);
+			if (!range[2]) {
+				for (let frame of frames) {
+					let column = frame % columns;
+					let row = Math.floor(frame / columns)
+					this.frameGroups[i][ii] = new Box(undefined, 
+						frameWidth * column, 
+						frameHeight * row, 
+						frameWidth, 
+						frameHeight
+					);
+					ii++
+				}
+			} else { //mirrored on x
+				for (let frame of frames) {
+					let column = frame % columns;
+					let row = Math.floor(frame / columns)
+					this.frameGroups[i][ii] = new Box(undefined, 
+						frameWidth * (column + 1), 
+						frameHeight * row, 
+						frameWidth * -1, 
+						frameHeight
+					);
+					ii++
+				}
+			}
+			i++;
+		}
+		this.currentFrame = this.frameGroups[this.currentFrameGroup][this.currentFrameIndex];
+	}
+	toFrame = (i) => {
+		this.currentFrameIndex = i;
+		this.currentFrame = this.frameGroups[currentFrameGroup][this.currentFrameIndex];
+	}
+	switchGroup = (i) => {
+		this.currentFrameGroup = i;
+		toFrame(0);
+	}
+	nextFrame = () => {
+		this.currentFrameIndex++;
+		if (this.currentFrameIndex == this.frameGroups[this.currentFrameGroup].length) {
+			this.currentFrameIndex = 0;
+		}
+		this.currentFrame = this.frameGroups[currentFrameGroup][this.currentFrameIndex];
+		if (this.currentFrameIndex = 0) {
+			return true;
+		}
+		return false;
+	}
+	previousFrame = () => {
+		this.currentFrameIndex--;
+		if (this.currentFrameIndex == -1) {
+			this.currentFrameIndex = this.frameGroups[this.currentFrameGroup].length - 1;
+		}
+		this.currentFrame = this.frameGroups[currentFrameGroup][this.currentFrameIndex];
+		if (this.currentFrameIndex = 0) {
+			return true;
+		}
+		return false;
+	}
+}
+class AnimatedSprite extends Sprite {
+	/*
+		A renderable object that supports animations, also used for players
+		ATTRIBUTES
+		ci-id=string - the ID of the sprite, to be used in the associative array for sprites
+		c-spriteSheet=SpriteSheet - the spritesheet to be used
+		ci-image=any canvas drawaable image - the image to be shown -- should be image stored in variable so it can be reused
+		ci-x=number - the x position
+		ci-y=number - the y position
+		ci-width=number - width of the image -- automatically set to image width if not defined
+		ci-height=number - height of the image -- automatically set to image height if not defined
+		i-parent=Scene - parent scene, not defined until added to scene using Scene.addSprite()
+		METHODS
+		render(context) - renders sprite to the context at Sprite.x and Sprite.y
+		i-move(dx, dy) - moves by dx and dy
+		i-moveTimed(dx, dy, time, interval) moves by 𝚫x/interval and 𝚫y/interval over time milliseconds -- uses move with setinterval and counter
+		i-moveTo(x, y) moves to x and y -- uses move
+		i-moveToTimed(x, y, time, interval) moves to x and y by 𝚫x/interval and 𝚫y/interval over time milliseconds -- uses movetimed
+		startAnimation(interval, reverse=bool) - starts the animation, returns true every time it reaches 0
+		stopAnimation() - removes the animation timer, returns This so it an be chained with toFrame
+		switchAnimation(frameGroup) - sets spritesheet frameGroup to specified number
+		toFrame(i, ii) - goes to frameGroup[i, ii] from spritesheet
+		loopAnimation(reps, interval, reverse) - goes through animation the specified amount of times
+	*/
+}
 
 //main functions
 const setCanvasBackground = (scene, canvas) => {
@@ -181,6 +306,10 @@ const renderLoop = (scene) => {
 window.addEventListener("load", 
 	() => {
 		let scene = new Scene(1000, 1000, "")
+		let h = new Image();
+		h.src = "./sample.png";
+		let j = new SpriteSheet(h, 28, 18, [0, 4], [0, 9], [5, 7], [0, 99, true])
+		console.log(j);
 		setCanvasBackground(scene, canvas);
 		renderLoop(scene);
 	}
